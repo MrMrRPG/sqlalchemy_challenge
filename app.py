b7e737abd443c4899f499aeb179de2ca9d12f8c0
MrMrRPG <67204447+MrMrRPG@users.noreply.github.com>
2021-08-24 00:52:41 -0700

    app.py need to be updated

--- app.py
+++ app.py
@@ -19,4 +19,21 @@ Measurement = Base.classes.measurement
 session = Session(engine)
 
 #Set up Flask
-app = Flask(name)
+app = Flask(__name__)
+
+###########################################################
+
+@app.route("/")
+def welcome():
+    return (
+        f"Welcome to the Hawaii Weather Station API!<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/<start><br/>"
+        f"/api/v1.0/<start>/<end><br/>"
+        )
+# @app.route("/")
+
+if __name__ == "__main__":
+	app.run(debug=True)
